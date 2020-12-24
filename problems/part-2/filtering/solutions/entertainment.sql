9764b4d821d5426054c1cb1bfe4d7e06e9d2c396
Konstantin Simeonov <kon.simeonov@protonmail.com>
2020-12-24 19:23:38 +0200

    Some more solutions

--- problems/part-2/filtering/solutions/entertainment.sql
+++ problems/part-2/filtering/solutions/entertainment.sql
@@ -5,4 +5,22 @@ SELECT
 FROM
     engagements
 WHERE
-    start_date <= '2012-10-31' and end_date >= '2012-10-01';
+    start_date <= '2012-10-31' AND end_date >= '2012-10-01';
+
+SELECT
+    *
+FROM
+    engagements
+WHERE
+    start_time BETWEEN '12:00' AND '17:00'
+    AND
+    start_date <= '2012-10-31'
+    AND
+    end_date >= '2012-10-01';
+
+SELECT
+    *
+FROM
+    engagements
+WHERE
+    start_date = end_date;
